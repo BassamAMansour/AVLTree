99b15d669a6c1528c297a2facdae1e4799111cbd
Ahmed Hussien <a7med.7ussien2011@gmail.com>
2018-03-20 20:37:34 +0200

    #dictinary

--- src/eg/edu/alexu/csd/filestructure/avl/Implementation/Dictionary.java
+++ src/eg/edu/alexu/csd/filestructure/avl/Implementation/Dictionary.java
@@ -1,37 +1,83 @@
 package eg.edu.alexu.csd.filestructure.avl.Implementation;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import eg.edu.alexu.csd.filestructure.avl.IDictionary;
 
-import java.io.File;
-
 public class Dictionary implements IDictionary {
+	
+	private AVLTree avl;
+	public Dictionary(){
+		avl = new AVLTree();
+	}
+	
     @Override
     public void load(File file) {
+    	BufferedReader br = null;
+		FileReader fr = null;
 
+		try {
+
+			//br = new BufferedReader(new FileReader(FILENAME));
+			fr = new FileReader(file);
+			br = new BufferedReader(fr);
+
+			String sCurrentLine;
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				avl.insert(sCurrentLine);
+			}
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+
+				if (br != null)
+					br.close();
+
+				if (fr != null)
+					fr.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+
+		}
     }
 
     @Override
     public boolean insert(String word) {
-        return false;
+    	if(exists(word)) return false;
+    	avl.insert(word);
+        return true;
     }
 
     @Override
     public boolean exists(String word) {
-        return false;
+        return avl.search(word);
     }
 
     @Override
     public boolean delete(String word) {
-        return false;
+        return avl.delete(word);
     }
 
     @Override
     public int size() {
-        return 0;
+        return avl.getNumberOfNodes();
     }
 
     @Override
     public int height() {
-        return 0;
+        return avl.height();
     }
 }
